1db0fee2b6248355d85a0559e68b8aaee3ada978
kitsune-turing <tobongoomez809@gmail.com>
2024-08-25 14:53:04 -0500

    feat(register): Add navigation from Register to Login activity

--- app/src/main/java/com/kitsune/android/u/auth/Register.java
+++ app/src/main/java/com/kitsune/android/u/auth/Register.java
@@ -2,15 +2,20 @@ package com.kitsune.android.u.auth;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class Register extends Activity {
+    private TextView loginButton;
 
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_register);
-    }
 
+        loginButton = findViewById(R.id.btn_sign_in_prompt);
+        loginButton.setOnClickListener(view -> startActivity(new Intent(Register.this, Login.class)));
+    }
 }
