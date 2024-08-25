a24773686bc3378685a33a932b3005f8ca2ee9a9
kitsune-turing <tobongoomez809@gmail.com>
2024-08-25 00:10:03 -0500

    feat: Implemented registration activity and password toggle functionality

---
+++ app/src/main/java/com/kitsune/android/u/auth/Register.java
@@ -0,0 +1,16 @@
+package com.kitsune.android.u.auth;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Register extends Activity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
