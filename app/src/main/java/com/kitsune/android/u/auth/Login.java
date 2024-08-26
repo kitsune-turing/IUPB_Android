95f4b840730a6eac9fd7ba40b5d23afeb752397c
kitsune-turing <tobongoomez809@gmail.com>
2024-08-25 20:19:12 -0500

    refactor: Enhance Login Activity with password visibility toggle and logging

--- app/src/main/java/com/kitsune/android/u/auth/Login.java
+++ app/src/main/java/com/kitsune/android/u/auth/Login.java
@@ -4,62 +4,49 @@ package com.kitsune.android.u.auth;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.PasswordTransformationMethod;
-import android.text.method.SingleLineTransformationMethod;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.kitsune.android.u.auth.Model.User;
+import com.kitsune.android.u.auth.utils.PasswordUtils;
 
 
 public class Login extends Activity {
     private EditText usernameEditText;
     private EditText passwordEditText;
     private ImageView togglePasswordButton;
-    private TextView loginButton;
-    private TextView registerButton;
-    private boolean isPasswordVisible = false;
+    private boolean isPasswordVisible = true;
 
+    // Components
+    PasswordUtils passwordUtils = new PasswordUtils();
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate called");
         setContentView(R.layout.activity_main);
 
         // Init components
         usernameEditText = findViewById(R.id.edit_username);
         passwordEditText = findViewById(R.id.edit_password);
         togglePasswordButton = findViewById(R.id.btn_password_toggle);
-        loginButton = findViewById(R.id.btn_sign_in);
-        registerButton = findViewById(R.id.btn_sign_up);
+        TextView loginButton = findViewById(R.id.btn_sign_in);
+        TextView registerButton = findViewById(R.id.btn_sign_up);
 
         // Listeners configurations
-        loginButton.setOnClickListener(v ->
-        {
+        loginButton.setOnClickListener(v -> {
             User user = new User(this);
             user.verifyCredentials(usernameEditText.getText().toString(),
                     passwordEditText.getText().toString());
         });
 
         registerButton.setOnClickListener(view -> startActivity(new Intent(Login.this, Register.class)));
-        togglePasswordButton.setOnClickListener(view -> togglePasswordVisibility());
-    }
 
-    private void submitLoginForm() {
-
-
-    }
-
-    private void togglePasswordVisibility() {
-        if (isPasswordVisible) {
-            isPasswordVisible = false;
-            passwordEditText.setTransformationMethod(new PasswordTransformationMethod());
-            togglePasswordButton.setImageResource(R.drawable.password_open);
-        } else {
-            isPasswordVisible = true;
-            passwordEditText.setTransformationMethod(new SingleLineTransformationMethod());
-            togglePasswordButton.setImageResource(R.drawable.password_close);
-        }
-        passwordEditText.setSelection(passwordEditText.length());
+        togglePasswordButton.setOnClickListener(view -> {
+            isPasswordVisible = !isPasswordVisible;
+            passwordUtils.togglePasswordVisibility(isPasswordVisible, passwordEditText, togglePasswordButton);
+        });
     }
 }
