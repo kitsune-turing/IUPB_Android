5781fcc2555a72862eee0d65d2760820dc6e9d98
kitsune-turing <tobongoomez809@gmail.com>
2024-09-09 00:54:54 -0500

    refactor: Improve PasswordUtils for toggling visibility

--- app/src/main/java/com/kitsune/android/u/auth/utils/PasswordUtils.java
+++ app/src/main/java/com/kitsune/android/u/auth/utils/PasswordUtils.java
@@ -1,6 +1,5 @@
 package com.kitsune.android.u.auth.utils;
 
-
 import android.text.method.PasswordTransformationMethod;
 import android.text.method.SingleLineTransformationMethod;
 import android.widget.EditText;
@@ -9,27 +8,31 @@ import android.widget.ImageView;
 import com.kitsune.android.u.auth.R;
 
 
+/**
+ * Utility class for handling password visibility toggle in EditText fields.
+ */
 public class PasswordUtils {
 
+
     /**
-     * Change visibility for password in the component EditText and update icon of button.
+     * Toggles the visibility of the password in an EditText field and updates the toggle button icon.
      *
-     * @param isPasswordVisible This is visibility for password in the moment.
-     * @param passwordEditText EditText contain password.
-     * @param togglePasswordButton ImageView is a manager change state the format for the EditText in the UI.
-     * @return Is a function void, not return value
+     * @param isPasswordVisible Current visibility state of the password.
+     * @param passwordEditText The EditText containing the password.
+     * @param togglePasswordButton The ImageView button used to toggle visibility.
      */
-    public void togglePasswordVisibility(boolean isPasswordVisible,
-                                          EditText passwordEditText, ImageView togglePasswordButton) {
+    public static void togglePasswordVisibility(boolean isPasswordVisible,
+                                                EditText passwordEditText, ImageView togglePasswordButton) {
         if (isPasswordVisible) {
-            isPasswordVisible = false;
-            passwordEditText.setTransformationMethod(new PasswordTransformationMethod());
-            togglePasswordButton.setImageResource(R.drawable.password_open);
-        } else {
-            isPasswordVisible = true;
+            // Set password to visible
             passwordEditText.setTransformationMethod(new SingleLineTransformationMethod());
             togglePasswordButton.setImageResource(R.drawable.password_close);
+        } else {
+            // Set password to hidden
+            passwordEditText.setTransformationMethod(new PasswordTransformationMethod());
+            togglePasswordButton.setImageResource(R.drawable.password_open);
         }
+        // Move the cursor to the end of the text
         passwordEditText.setSelection(passwordEditText.length());
     }
 }
