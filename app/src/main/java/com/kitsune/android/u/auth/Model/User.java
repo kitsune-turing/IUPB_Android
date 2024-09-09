13eaacf1fd5107206c39b4ab8ac0344d795d5f90
kitsune-turing <tobongoomez809@gmail.com>
2024-09-09 00:56:48 -0500

    refactor: Update User model to align with Firebase implementation

--- app/src/main/java/com/kitsune/android/u/auth/Model/User.java
+++ app/src/main/java/com/kitsune/android/u/auth/Model/User.java
@@ -1,20 +1,30 @@
 package com.kitsune.android.u.auth.Model;
 
 
-import android.content.Context;
-import com.kitsune.android.u.auth.Configuration.DataBase;
-
-
 public class User {
+    private String uid;
+    private String fullName;
     private String email;
-    private String pass;
-    private DataBase db;
+    private String phone;
 
-    public User(Context context) {
-        this.db = new DataBase(context);
+    public User() { }
+
+    public User(String uid, String fullName, String email, String phone) {
+        this.uid = uid;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
     }
 
-    public void verifyCredentials(String email, String password) {
-        db.verifyUser(email, password);
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
     }
 }
