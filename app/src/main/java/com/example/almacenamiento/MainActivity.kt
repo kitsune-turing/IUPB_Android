15c1c37f5c9955f207c9bde1ec35a4e0c336636f
Kitsune <tobongoomez809@gmail.com>
2024-10-27 15:00:50 -0500

    docs: improve documentation for MainActivity

--- app/src/main/java/com/example/almacenamiento/MainActivity.kt
+++ app/src/main/java/com/example/almacenamiento/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.almacenamiento
 
-
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -9,28 +8,33 @@ import com.example.almacenamiento.ui.theme.AlmacenamientoTheme
 import com.example.almacenamiento.components.ShowData
 import com.example.almacenamiento.data.DataManager
 
-
+/**
+ * MainActivity is the entry point of the application.
+ * It manages the main user interface and handles data storage and retrieval.
+ */
 class MainActivity : ComponentActivity() {
-    private val TAG = "MainActivity"
 
+    /**
+     * onCreate is called when the activity is first created.
+     * It initializes the activity, sets up the user interface, 
+     * and handles data management using SharedPreferences.
+     *
+     * @param savedInstanceState A Bundle containing the activity's previously saved state.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i(TAG, "onCreate: Iniciando MainActivity")
-
-        // Guarda los datos en SharedPreferences desde el archivo.
+        Log.i("MainActivity_onCreateMain", "onCreate: Starting MainActivity")
         DataManager.saveDataSharedPreferences(this)
 
-        // Configura la interfaz con Jetpack Compose.
+        // Set the user interface using Jetpack Compose
         setContent {
             AlmacenamientoTheme {
-                // Recupera los datos y los muestra en la pantalla.
                 val co2Data = DataManager.getDataSharedPreferences(applicationContext)
-                Log.i(TAG, "onCreate: Datos recuperados: ${co2Data.size} registros")
 
-                co2Data.forEach { (fecha, co2) ->
-                    Log.d(TAG, "Registro -> Fecha: $fecha - CO2: $co2 ppm")
+                Log.i("MainActivity_GetDatum_DataManager", "onCreate: Retrieved data: ${co2Data.size} records")
+                co2Data.forEach { (date, co2) ->
+                    Log.d("MainActivity_RegisterDatum_DataManager", "Record -> Date: $date - CO2: $co2 ppm")
                 }
-
                 ShowData(co2Data)
             }
         }
